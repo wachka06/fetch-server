bb976ab594340bc29ffb0c9a162c6ecd9ef6e40c
Toledo <gabriel.toledo@accenture.com>
2020-03-04 17:11:37 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`Woohoo! We are good to go on ${url}!`);
+});
