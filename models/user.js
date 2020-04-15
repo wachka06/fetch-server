7b3830c763de1ddbfadae09db75b764849c2292c
Toledo <gabriel.toledo@accenture.com>
2020-04-15 11:51:55 -0700

    User model fields declared twice

--- models/user.js
+++ models/user.js
@@ -83,42 +83,7 @@ module.exports = (sequelize, DataTypes) => {
           updatedUser.longitude = longitude;
         },
       },
-      pet_age_preference: {
-        type: DataTypes.ENUM(PetPreferences.PET_AGE),
-      },
-      pet_type_preference: {
-        type: DataTypes.ENUM(PetPreferences.PET_TYPE),
-      },
-      pet_distance_preference: {
-        type: DataTypes.INTEGER,
-      },
-      pet_dependency_preference: DataTypes.ENUM(PetPreferences.PET_DEPENDENCY),
-      pet_activity_preference: DataTypes.ENUM(PetPreferences.PET_ACTIVITY),
-      pet_trainability_preference: DataTypes.ENUM(
-        PetPreferences.PET_TRAINABILITY
-      ),
-      pet_social_preference: DataTypes.ENUM(PetPreferences.PET_SOCIABILITY),
-      pet_sex_preference: {
-        type: DataTypes.ENUM(PetPreferences.PET_SEX),
-      },
-      pet_experience_level: {
-        type: DataTypes.ENUM(PetPreferences.PET_EXPERIENCE_LEVEL),
-      },
-      pet_good_with_children_preference: DataTypes.BOOLEAN,
-      pet_good_with_dogs_preference: DataTypes.BOOLEAN,
-      pet_good_with_cats_preference: DataTypes.BOOLEAN,
     },
-    {
-      hooks: {
-        beforeCreate: async (newUser) => {
-          const { latitude, longitude } = await locationToCoordinates({
-            zipcode: newUser.zipcode,
-          });
-          newUser.latitude = latitude;
-          newUser.longitude = longitude;
-        },
-      },
-    }
   );
   user.associate = function (models) {
     user.hasMany(models.liked_pet, {
